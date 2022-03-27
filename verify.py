f23b8f3a6b2cb6789f0572a8a6e831219b5dcc50
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2022-03-27 22:49:20 +0530

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,9 +1,20 @@
+#Created : 27 Mar 2022 
+# This tries to read all the images in a dir 
+# and gives an error when it is not able to open ( mostly because its corrupted while downloading )
+
+# To do : Display the error / exception type / message 
+
 import os
 from PIL import Image
-DIR = '../images/Uninfected/'
-for filename in os.listdir(DIR):
-  img = Image.open(DIR+filename)
-	  try:
-		  img.verify()
-		except e1 as Exception  :
-			print("Exception:" + str(e1))	
+
+DIR = '../data/eval_all/'
+
+try:
+  for filename in os.listdir(DIR):
+    img = Image.open(DIR+filename)
+    img.verify()
+except : # e1 as Exception :
+  #print("Exception:" + str(e1)) 
+  print(filename)
+
+
